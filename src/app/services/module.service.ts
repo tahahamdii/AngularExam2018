0e3b3fd9bdbf6016d862b0cf80fbba8d3efd0b2f
Med Taha Hamdi <hamdi.taha@esprit.tn>
2024-01-09 02:02:41 +0100

    final

---
+++ src/app/services/module.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ModuleService {
+  private apiUrl = 'http://localhost:3000'; // Removed trailing slash
+
+  constructor(private http: HttpClient) { }
+  getModules(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  addModule(module: any): Observable<any> {
+    const addModuleUrl = `${this.apiUrl}/modules`;
+    return this.http.post(addModuleUrl, module);
+  }
+}
